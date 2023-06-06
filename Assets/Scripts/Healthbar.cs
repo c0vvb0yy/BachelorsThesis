70426d9f7d45d46fbb2c8c0c848a86adb90212fa
covvboyy <jamison.bassett@gmail.com>
2023-06-06 13:01:42 +0200

    animating healthbar

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] Image _healthbarSprite;
+    [SerializeField] float reduceSpeed = 1.2f;
+    float _target = 1f;
 
     public void UpdateHealthbar(float maxHealth, float currentHealth){
-        _healthbarSprite.fillAmount = currentHealth / maxHealth;
+        _target = currentHealth / maxHealth;
+    }
+
+    private void Update() {
+        _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, reduceSpeed*Time.deltaTime);
     }
 }
