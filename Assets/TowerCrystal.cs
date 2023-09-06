6711aa9871162875f0c80153f0f0059623efe87f
covvboyy <jamison.bassett@gmail.com>
2023-09-06 18:55:44 +0200

    Asset import

---
+++ Assets/TowerCrystal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCrystal : MonoBehaviour
+{
+    [SerializeField] float _spinSpeed = 1f;
+    [SerializeField] float _floatSpeed = 2f;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        LeanTween.reset();
+    }
+    void OnEnable(){
+        LeanTween.rotateAround(this.gameObject, Vector3.up, 360, _spinSpeed).setLoopClamp();
+        LeanTween.moveLocalY(this.gameObject, 0.1f, _floatSpeed).setEaseInOutSine().setLoopPingPong();
+    }
+}
