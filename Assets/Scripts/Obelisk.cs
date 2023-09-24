2ea547b9864b0b0f439e1b5101198ad10ff79635
covvboyy <jamison.bassett@gmail.com>
2023-09-24 16:31:04 +0200

    added visual feedback for obelisk activation

--- Assets/Scripts/Obelisk.cs
+++ Assets/Scripts/Obelisk.cs
@@ -7,20 +7,30 @@ using Unity.Services.Analytics;
 public class Obelisk : MonoBehaviour
 {
     bool _activated;
-    
+    public float spinSpeed;
+    public float floatSpeed;
+    public GameObject tinyPillars;
+    public GameObject mainPillar;
     public static event Action OnActivation;
 
     public void Activate(){
         if(!_activated){
-            Debug.Log("activating....");
-            OnActivation?.Invoke();
+            OnActivation.Invoke();
             var eventData = new Dictionary<string, object>{
                 {"Obelisk", gameObject.name}
             };
             AnalyticsService.Instance.CustomData("Obelisk", eventData);
         
             Debug.Log("obelisk: " + gameObject.name + " activated");
+            StartTweens();
             _activated = true;
         }
+    }
+
+
+    void StartTweens(){
+        LeanTween.rotateAround(tinyPillars, Vector3.up, 360, spinSpeed);
+        LeanTween.rotateAround(mainPillar, Vector3.up, 360, spinSpeed);
+        LeanTween.moveLocalY(mainPillar, 1.5f, floatSpeed).setEaseInOutSine().setLoopPingPong();
     }
 }
